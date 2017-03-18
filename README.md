9f0dd676b672ceca9165579a211147fe39d8ee0f
Pulkit Sinha <itsapinhulk@live.in>
2017-03-17 22:22:56 -0400

    Add version file

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-<coming soon>
+PyRepeat
+========
 
-- [ ] setup test
+Python script which repeats input command until stopped.
+
+This personal project also serves as a way to learn package distribution using pip.
+
+Project code hosted at : https://github.com/itsapinhulk/pyrepeat
+
+TODO
+----
+- [x] Add LICENCE, README, etc
+- [ ] Add package distribution
+- [ ] Setup tests
--- repeat.py
+++ repeat.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
+import os
 import time
 import argparse
 import datetime
 import subprocess
+
+# Major version, minor version
+version = [int(x) for x in open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'VERSION'), 'r').read().split('.')]
 
 def _repeat_command(num_repeat, repeat_interval, no_summary, no_clear, command) :
   times_run = 0
@@ -13,7 +17,7 @@ def _repeat_command(num_repeat, repeat_interval, no_summary, no_clear, command) 
 
     if not no_summary :
       echo_summary = ' '.join(command)
-      print('Running "{0:s}" every {1:d} seconds at {2:s}'.format(echo_summary, repeat_interval,
+      print('[Running "{0:s}" every {1:d} seconds at {2:s}]'.format(echo_summary, repeat_interval,
                                          datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
 
     subprocess.call(command)
@@ -22,6 +26,7 @@ def _repeat_command(num_repeat, repeat_interval, no_summary, no_clear, command) 
     times_run += 1
 
 def _parse_args() :
+  # parameters are prefixed with --r to avoid collision with command being called
   args = argparse.ArgumentParser(description = 'Repeat command until stopped')
   args.add_argument('--r-num', action='store', dest='num_repeat', default=0, type=int, metavar='NUMBER',
                     help='Number of times to repeat command. Defaults to 0, which repeats forever.')
@@ -29,14 +34,17 @@ def _parse_args() :
                     help='Sleep interval between command repetition in seconds, Defaults to 60.')
   args.add_argument('--r-no-clear', action='store_true', dest='noclear', help='Do not clear screen between successive commands')
   args.add_argument('--r-no-summary', action='store_true', dest='nosummary', help='Do not show command summary before execution')
+  args.add_argument('--r-version', action='store_true', dest='showversion', help='Show version number and exit')
 
   return args.parse_known_args()
 
 def _main() :
   (args, rem_cmd) = _parse_args()
-  _repeat_command(num_repeat = args.num_repeat, repeat_interval = args.repeat_interval,
-                  no_summary = args.nosummary, no_clear = args.noclear, command = rem_cmd)
-
+  if args.showversion :
+    print('Repeat ' + '.'.join([str(x) for x in list(version)]))
+  else :
+    _repeat_command(num_repeat = args.num_repeat, repeat_interval = args.repeat_interval,
+                    no_summary = args.nosummary, no_clear = args.noclear, command = rem_cmd)
 
 if '__main__' == __name__ :
   _main()
